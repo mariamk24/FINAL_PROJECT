b2fc119a07e8c2800cf9226ce9fcc36ac47b2608
Mariam Khan <seaotter83@gmail.com>
2015-07-09 17:58:55 +0000

    Added home page

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,15 @@
 require 'bundler'
 Bundler.require
-
+require_relative "./models/restaurant.rb"
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :home
+  end
+  get '/restaurant' do
+    erb :restaurant
+  end
+  post '/restaurant' do
   end
 
 end

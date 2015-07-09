3c5c1bd2ef56c2a0cb1b804a088a71b79ec50e31
Noah Gundotra <noah@gundotra.org>
2015-07-09 18:00:19 +0000

    add ruby logic to split choice param

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,23 @@
 require 'bundler'
 Bundler.require
-
+require_relative './models/restaurant.rb'
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :home
   end
-
+  
+  post '/restaurant' do
+    @foodchoice = params[:foodchoice].split(" ")
+    @foodchoice = @foodchoice[rand(@foodchoice.length)]
+    erb :displaychoice
+  end
+  
+  get '/restaurant' do
+    erb :restaurant
+  end
+  
+  get '/displaychoice' do
+    erb :displaychoice
+  end
 end

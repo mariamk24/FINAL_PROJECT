0af6d4c56e84dd85dd82428228f9fc4eb93141ef
Mariam Khan <seaotter83@gmail.com>
2015-07-09 19:44:54 +0000

    added the rest of front-end

--- application_controller.rb
+++ application_controller.rb
@@ -11,5 +11,15 @@ class ApplicationController < Sinatra::Base
   end
   post '/restaurant' do
   end
+  get '/food-types' do
+    erb :food_types
+  end
+  post '/food-types' do
+  end
+  get '/food-specs' do
+    erb :food_spec
+  end
+  post '/food-spes' do
+  end
 
 end
